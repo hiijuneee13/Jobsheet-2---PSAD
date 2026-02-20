b105ff028214cef4b936d5437abba07a4d13caf1
Rizal Maulana <xjunngoat1@gmail.com>
2026-02-20 08:03:54 +0700

    Percobaan 2

--- MahasiswaMain25.java
+++ MahasiswaMain25.java
@@ -7,8 +7,8 @@ public class MahasiswaMain25 {
         mhs1.ipk = 3.55;
 
         mhs1.tampilkanInformasi();
-        mhs1.ubahKelas("SI 2K ");
-        mhs1.updateIpk(3.6);
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
     }
 }
