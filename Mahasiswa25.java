ebcecf75c76643d6c61c06645c554f15b9cae31b
Rizal Maulana <xjunngoat1@gmail.com>
2026-02-20 08:11:34 +0700

    Percobaan 1

--- Mahasiswa25.java
+++ Mahasiswa25.java
@@ -5,10 +5,10 @@ public class Mahasiswa25 {
     double ipk;
 
     void tampilkanInformasi() {
-        System.out.println("NIM   : " + nim);
         System.out.println("Nama  : " + nama);
-        System.out.println("Kelas : " + kelas);
+        System.out.println("NIM   : " + nim);
         System.out.println("IPK   : " + ipk);
+        System.out.println("Kelas : " + kelas);
     }
 
     void ubahKelas(String kelasBaru) {
